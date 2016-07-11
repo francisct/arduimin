559824b09a56286a56af8a7144ba906056f83701
francis cote-tremblay <franciscotetremblay@gmail.com>
2016-07-11 14:24:40 -0400

    added soundtouch lib

--- HighLevel/ThereminPlayer/ThereminPlayer/Player.cs
+++ HighLevel/ThereminPlayer/ThereminPlayer/Player.cs
@@ -3,26 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CoreAudioApi;
 
 namespace ThereminPlayer
 {
-    private readonly MMDeviceEnumerator _deviceEnumerator = new MMDeviceEnumerator();
-    private readonly MMDevice _playbackDevice;
 
     class Player
     {
-        public Player(float volume, float pitch)
+
+
+
+        public Player(string wav)
         {
-
-        }
-
-        public void SetVolume(int volumeLevel)
-        {
-            if (volumeLevel < 0 || volumeLevel > 100)
-                throw new ArgumentException("Volume must be between 0 and 100!");
-
-            _playbackDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volumeLevel / 100.0f;
         }
     }
 }
